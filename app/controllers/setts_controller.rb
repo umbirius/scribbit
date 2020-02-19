5b332b9f0f980f056f25f5de4ebaf66fdf2d7eb8
umberto palazzo <upalazzo93@gmail.com>
2020-02-18 21:51:32 -0500

    added edit update destroy

--- app/controllers/setts_controller.rb
+++ app/controllers/setts_controller.rb
@@ -17,6 +17,28 @@ class SettsController < ApplicationController
         @sett = Sett.find(params[:id])
     end
 
+    def edit 
+        @sett = Sett.find(params[:id])
+
+    end 
+
+    def update 
+        @sett = Sett.find(params[:id])
+        @sett.update(setts_params)
+        if @sett.save
+            redirect_to project_sett_url(params[:project_id], @sett )
+        else 
+            render :edit
+        end 
+    end
+
+    def destroy 
+        @sett = Sett.find(params[:id])
+        @sett.destroy 
+        flash[:notice] = "#{@sett.name} has been deleted"
+        redirect_to project_setts_path(params[:project_id])
+    end
+
     private
 
     def setts_params
