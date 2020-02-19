d634ce7ca612c4b4b08a36237d05718e8c78e405
umberto palazzo <upalazzo93@gmail.com>
2020-02-18 21:47:51 -0500

    added update destroy edit

--- app/controllers/scenes_controller.rb
+++ app/controllers/scenes_controller.rb
@@ -18,6 +18,27 @@ class ScenesController < ApplicationController
         @scene = Scene.find(params[:id])
     end
 
+    def edit 
+        @scene = Scene.find(params[:id])
+
+    end 
+
+    def update 
+        @scene = Scene.find(params[:id])
+        @scene.update(scenes_params)
+        if @scene.save
+            redirect_to project_scene_url(params[:project_id], @scene )
+        else 
+            render :edit
+        end 
+    end
+
+    def destroy 
+        @scene = Scene.find(params[:id])
+        @scene.destroy 
+        flash[:notice] = "#{@scene.title} has been deleted"
+        redirect_to project_scenes_path(params[:project_id])
+    end
     private 
     def scenes_params
         params.require(:scene).permit(:title, :description, :order)
