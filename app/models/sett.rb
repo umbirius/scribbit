83b982792ec02d8072ae660692a25be6a6f3b2d9
umberto palazzo <upalazzo93@gmail.com>
2020-02-23 11:03:21 -0500

    added scope sort to setting

--- app/models/sett.rb
+++ app/models/sett.rb
@@ -3,4 +3,11 @@ class Sett < ApplicationRecord
     validates :name, presence:true, uniqueness:true
     validates :location, presence:true
     validates :description, presence:true
+
+    scope :sort_by_type, -> (type) {reorder("LOWER(#{type})" +" "+ "asc")}
+
+    def self.sort_by_type(type)
+        (reorder("LOWER(#{type})" +" "+ "asc"))
+    end 
+    
 end
