b7c4ea1a7c183881adf9bb29732a3167ca0a415a
umberto palazzo <upalazzo93@gmail.com>
2020-02-23 14:55:16 -0500

    create accept action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
     def index 
         @sub_reviews = current_user.requested_reviews
+        @acc_reviews = current_user.accepted_reviews
         byebug
         @req_reviews = Review.where.not(accepted: true, reviewee_id: 2)
     end
@@ -16,6 +17,12 @@ class ReviewsController < ApplicationController
         @review.save
         redirect_to reviews_path
     end
+
+    def accept 
+        @review = Review.find(params[:id])
+        @review.reviewer = current_user
+        redirect_to reviews_path
+    end 
 
     private 
 
