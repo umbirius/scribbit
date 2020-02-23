1d1f9662fdb6b8b7566e025c906e41f2e6b8c038
umberto palazzo <upalazzo93@gmail.com>
2020-02-23 12:38:25 -0500

    review creation w proj and user association

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,7 +9,17 @@ class ReviewsController < ApplicationController
 
     def create
         byebug
-        @review = current_user.build(review_params)
+        @review = current_user.requested_reviews.build()
+        @project = Project.find_by(title: params[:review][:project])
+        @review.project = @project
+        @review.save
+        redirect_to reviews_path
     end
+
+    private 
+
+    def review_params
+        params.require(:review).permit()
+    end 
 
 end
