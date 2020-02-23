03cd677a86dabcd312fe6a06e7b8e4ec9c9ccfbc
umberto palazzo <upalazzo93@gmail.com>
2020-02-23 12:02:31 -0500

    added route for peee review and migrated

--- db/migrate/20200223164611_create_reviews.rb
+++ db/migrate/20200223164611_create_reviews.rb
@@ -3,6 +3,8 @@ class CreateReviews < ActiveRecord::Migration[5.2]
     create_table :reviews do |t|
       t.integer :reviewer_id
       t.integer :reviewee_id
+      t.boolean :accepted
+      t.boolean :review_status
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_231551) do
+ActiveRecord::Schema.define(version: 2020_02_23_164611) do
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
@@ -50,6 +50,15 @@ ActiveRecord::Schema.define(version: 2020_02_19_231551) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "reviewer_id"
+    t.integer "reviewee_id"
+    t.boolean "accepted"
+    t.boolean "review_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "scenes", force: :cascade do |t|
