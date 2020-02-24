c7a1b7ef72d3d49af1244970fb73bbd939851281
umberto palazzo <upalazzo93@gmail.com>
2020-02-23 20:56:57 -0500

    application layout update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,16 @@
 require_tree .
 
 $(function(){ $(document).foundation(); });
+
+$(function() {
+    $(window).scroll(function() {
+      var winTop = $(window).scrollTop();
+      if (winTop >= 30) {
+        $("body").addClass("sticky-shrinknav-wrapper");
+      } else{
+        $("body").removeClass("sticky-shrinknav-wrapper");
+      }
+    });
+  });
+  
+  
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,20 @@
 class PostsController < ApplicationController
     def new 
+        @post = Post.new
     end 
 
     def create
+        @post = current_user.posts.build(post_params)
+        if @post.save 
+            redirect_to root_url
+        else 
+            render :new 
+        end
     end 
+
+    private
+
+    def post_params
+        params.require(:post).permit(:name, :description, :link)
+    end
 end
