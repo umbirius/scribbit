9031519553d868b980e25a0036504988588b37a3
umberto palazzo <upalazzo93@gmail.com>
2020-02-26 23:01:42 -0500

    validator to scope project

--- app/models/scene.rb
+++ app/models/scene.rb
@@ -1,6 +1,6 @@
 class Scene < ApplicationRecord
     belongs_to :project
-    validates :title, presence:true, uniqueness:true
+    validates :title, presence:true, uniqueness: {scope: :project}
     validates :description, presence:true
     validates :order, uniqueness: {scope: :project}, numericality: true, allow_nil:true 
     
